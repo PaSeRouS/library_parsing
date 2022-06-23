c7339a7a153886a62b6290bad4597cee05a8de35
PaSeRouS <paser96@mail.ru>
2022-06-23 22:40:14 +0300

    Add 4 arguments
    Add arguments:
    --dest_folder
    --skip_txt
    --skip_imgs
    --json_path

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,8 +1,11 @@
 import argparse
 import json
+import os
 
 import requests
 from bs4 import BeautifulSoup
+from pathlib import Path
+from pathvalidate import sanitize_filename
 from requests import ConnectionError, HTTPError
 from urllib.parse import urljoin
 
@@ -14,6 +17,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Парсер книг из большой бесплатной библиотеки'
     )
+
     parser.add_argument(
         '--start_page',
         help='Номер страницы, с которой скачивать',
@@ -25,8 +29,35 @@ if __name__ == '__main__':
         help='Номер страницы, на которой скачивание закончить',
         type=int
     )
+
+    parser.add_argument(
+        '--dest_folder',
+        help='Путь к папке с результатами парсинга'
+    )
     
+    parser.add_argument(
+        '--skip_imgs',
+        help='Не скачивать картинки',
+        action="store_true"
+    )
+
+    parser.add_argument(
+        '--skip_txt',
+        help='Не скачивать текстовые версии книг',
+        action="store_true"
+    )
+
+    parser.add_argument(
+        '--json_path',
+        help='Путь для файла books.json'
+    )
+
     args = parser.parse_args()
+
+    if args.dest_folder:
+        dest_folder = args.dest_folder
+    else:
+        dest_folder = ''
 
     if args.start_page:
         start_page = args.start_page - 1
@@ -52,7 +83,6 @@ if __name__ == '__main__':
 
         for book in books:
             book_url = urljoin('https://tululu.org', book.find('a')['href'])
-            print(book_url)
             book_page_response = requests.get(book_url)
             book_page_response.raise_for_status()
 
@@ -73,13 +103,30 @@ if __name__ == '__main__':
             if download_url:
                 books_descriptions.append(book_params)
 
-                download_txt(download_url, book_params['title'])
-                download_image(
-                    book_params['image_url'],
-                    book_params['image_name']
-                )
+                if not args.skip_txt:
+                    download_txt(
+                        download_url,
+                        book_params['title'],
+                        folder=f'{dest_folder}/books'
+                    )
+                
+                if not args.skip_imgs:
+                    download_image(
+                        book_params['image_url'],
+                        book_params['image_name'],
+                        folder=f'{dest_folder}/images'
+                    )
 
-    with open("books.json", "w", encoding='utf8') as json_file:
+    if args.json_path:
+        os.makedirs(args.json_path, exist_ok=True)
+        json_filename = Path(args.json_path, 'books.json')
+    elif not dest_folder:
+        json_filename = 'books.json'
+    else:
+        os.makedirs(dest_folder, exist_ok=True)
+        json_filename = Path(dest_folder, 'books.json')
+
+    with open(json_filename, "w", encoding='utf8') as json_file:
         json.dump(
             books_descriptions, 
             json_file, 
